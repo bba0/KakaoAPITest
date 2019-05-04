4da4676629be2d0280b9c0f4fdc41e875e629879
Park.hongchul <>
2019-05-04 19:27:29 +0900

    Blog Api와 Cafe Api를 하나로 합친다

--- app/src/main/java/com/kakaoapitest/ui/main/MainPresenter.kt
+++ app/src/main/java/com/kakaoapitest/ui/main/MainPresenter.kt
@@ -4,15 +4,41 @@ import android.util.Log
 import com.kakaoapitest.model.Document
 import com.kakaoapitest.network.Api
 import io.reactivex.Observable
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainPresenter(override var mVie: MainContract.View) : MainContract.Presenter {
     override fun search(query: String) {
-        searchBlog(query)
-        searchCafe(query)
+        var observableList = ArrayList<Observable<List<Document>>>()
+        observableList.add(searchBlog(query))
+        observableList.add(searchCafe(query))
+        Observable.zip(observableList) {
+            it
+        }.map {
+            Arrays.asList(*it)
+        }.flatMap {
+            Observable.fromIterable(it)
+        }
+            .map {
+                it as ArrayList<Document>
+            }
+            .flatMap {
+                Observable.fromIterable(it)
+            }
+            .toList()
+            .toObservable()
+            .compose(Api.transformerIOMainThread())
+            .subscribe({
+                Log.e("lol", it.toString())
+            }, {
+                Log.e("lol", it.message)
+            }, {
+
+            })
     }
 
-    private fun searchBlog(query: String) {
-        Api.searchBlog(query)
+    private fun searchBlog(query: String): Observable<List<Document>> {
+        return Api.searchBlog(query)
             .flatMap {
                 Observable.fromIterable(it.documents)
             }
@@ -21,19 +47,11 @@ class MainPresenter(override var mVie: MainContract.View) : MainContract.Present
             }
             .toList()
             .toObservable()
-            .compose(Api.transformerIOMainThread())
-            .subscribe({
-                Log.e("lol", it.toString())
-            }, {
-                Log.e("lol", it.toString())
-            }, {
-
-            })
     }
 
 
-    private fun searchCafe(query: String) {
-        Api.searchCafe(query)
+    private fun searchCafe(query: String): Observable<List<Document>> {
+        return Api.searchCafe(query)
             .flatMap {
                 Observable.fromIterable(it.documents)
             }
@@ -42,14 +60,6 @@ class MainPresenter(override var mVie: MainContract.View) : MainContract.Present
             }
             .toList()
             .toObservable()
-            .compose(Api.transformerIOMainThread())
-            .subscribe({
-                Log.e("lol", it.toString())
-            }, {
-                Log.e("lol", it.toString())
-            }, {
-
-            })
     }
 
 }
