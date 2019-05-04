fdbcff9d553f631cb321ef3aef01d164fc212565
Park.hongchul <>
2019-05-04 15:34:16 +0900

    Api Model을 만든다

--- app/src/main/java/com/kakaoapitest/model/BlogModel.kt
+++ app/src/main/java/com/kakaoapitest/model/BlogModel.kt
@@ -1,5 +1,16 @@
 package com.kakaoapitest.model
 
-data class BlogModel(override var url: String? = "") : Document() {
+import com.google.gson.annotations.SerializedName
+import java.util.*
 
+data class BlogModel(@SerializedName("url") override var url: String? = "") : Document() {
+    @SerializedName("title")
+    override var title: String? = null
+    @SerializedName("blogname")
+    override var name: String? = null
+    @SerializedName("thumbnail")
+    override var imageUrl: String? = null
+    override var label: String = "Blog"
+    @SerializedName("datetime")
+    override var date: Date? = null
 }
--- app/src/main/java/com/kakaoapitest/model/Document.kt
+++ app/src/main/java/com/kakaoapitest/model/Document.kt
@@ -1,5 +1,12 @@
 package com.kakaoapitest.model
 
+import java.util.*
+
 abstract class Document {
     open var url: String? = null
+    open var title: String? = null
+    open var name: String? = null
+    open var imageUrl: String? = null
+    open var label: String = "Document"
+    open var date: Date? = null
 }
